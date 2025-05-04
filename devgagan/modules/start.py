80e974126d61fb0cd303888e3742774c84c5cef4
rambhakt1-star <rambhakthoonme@gmail.com>
2025-05-04 07:57:19 +0530

    Update start.py

--- devgagan/modules/start.py
+++ devgagan/modules/start.py
@@ -192,11 +192,28 @@ async def terms(client, message):
 @app.on_message(filters.command("plan") & filters.private)
 async def plan(client, message):
     plan_text = (
-        "> 💰 **Premium Price**:\n\n Starting from $2 or 200 INR accepted via **__Amazon Gift Card__** (terms and conditions apply).\n"
-        "📥 **Download Limit**: Users can download up to 100,000 files in a single batch command.\n"
-        "🛑 **Batch**: You will get two modes /bulk and /batch.\n"
-        "   - Users are advised to wait for the process to automatically cancel before proceeding with any downloads or uploads.\n\n"
-        "📜 **Terms and Conditions**: For further details and complete terms and conditions, please send /terms.\n"
+        "> 💰 **Premium Price**:\n\n 💎 Upgrade to Premium 💎
+
+🚀 Exclusive Premium Features:  
+✅ No need to verify every 3 hours ⏳  
+✅ Bulk mode: Upload up to 300 files 📂  
+✅ No 300-second waiting time
+
+🔟 10-Day Plan  
+💰 Price: Rs 20\ 🇮🇳  
+⏳ Duration: 10 days  
+
+💰 20-Day Plan  
+💰 Price: Rs 40\ 🇮🇳  
+⏳ Duration: 20 days  
+
+🪙 Monthly Plan  
+💰 Price: Rs 60\ 🇮🇳  
+⏳ Duration: 30 days  
+
+📌 Payment Methods:  
+- payment No: 9329548617 📲  (terms and conditions apply).\n"
+        "📥 **Download Limit**: Users can download up to 500 files in a single batch command.\n"
     )
      
     buttons = InlineKeyboardMarkup(
