3526e8b713559140e49177fb18a3369273e1e49e
rambhakt1-star <rambhakthoonme@gmail.com>
2025-07-29 19:12:10 +0530

    Update func.py

--- devgagan/core/func.py
+++ devgagan/core/func.py
@@ -42,7 +42,7 @@ async def subscribe(app, message):
             await message.reply_text("You are Banned. Contact -- @sonuporsa")
             return 1
       except UserNotParticipant:
-        caption = f"Due To Heavy Load , Only Our Channel Members Can Use the bot 😊"
+        caption = f"Please Join Our Channel To use The bot 😊"
         await message.reply_photo(photo="https://ar-hosting.pages.dev/1752942112328.jpg",caption=caption, reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Join Now...", url=f"{url}")]]))
         return 1
       except Exception:
